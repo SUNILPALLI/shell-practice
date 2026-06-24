2e318eeea1f0b1f64002f7d5e73a7fee9879b3ea
sunil_palli <pallisunil49@gmail.com>
2026-06-24 11:34:27 +0530

    shell sunil

---
+++ data_types.sh
@@ -0,0 +1,12 @@
+num1=10
+num2=sunil
+
+sum=$(($num1+$num2))
+
+echo "sum is = $sum"
+
+#array
+parts=("eye","head","nose")
+echo "parts are : ${parts[@]}"
+echo "first part is : ${parts[1]}"
+echo "second part is : ${parts[2]}"
