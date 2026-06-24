c8677434776d006c0f5b91728c19895b2962c03f
sunil_palli <pallisunil49@gmail.com>
2026-06-24 11:54:31 +0530

    shell sunil

---
+++ conditions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+number=$1
+
+# gt - greater than
+# lt - less than
+# eq - equal 
+# ne - not equal
+# ge - greater than or equal
+# le - less than or equal 
+
+if [ $number -gt 100 ]; then
+    echo "given number $number is greater to 100"
+elif [ $number -eq 100]; then
+    echo "given number $number is equal to 100"
+else
+    echo "given number $number is less to 100"
+if
