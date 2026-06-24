fb02359f54d2f8c6ad1d491de76c39f1de01e896
sunil_palli <pallisunil49@gmail.com>
2026-06-24 12:19:22 +0530

    shell sunil

---
+++ install_functions.sh
@@ -0,0 +1,20 @@
+userid=$(id -u)
+
+#check root access or not
+if [$userid -ne 0]; then 
+    echo "please ru this script with root access"
+    exit 1
+fi
+
+dnf list installed mysql
+if [$? -eq 0]; then 
+    echo "mysql is already installed... skipping"
+else
+    echo "mysql is installing.."
+    dnf install mysql -y
+    if [$? -ne 0]; then
+        echo "mysql intallation ... failed"
+    else
+        echo "mysql installation... success"
+    fi
+fi
